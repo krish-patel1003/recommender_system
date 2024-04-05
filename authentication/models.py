d653c0e1dfa01ad2114c43588f27f2558b14e60d
Krish Patel <patel.krish1003@gmail.com>
2024-04-05 16:43:14 +0530

    initial setup and auth models added

---
+++ authentication/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+
+
+class CustomUserManager(BaseUserManager):
+
+    def __create(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('The username must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', False)
+        return self.__create(username, password, **extra_fields)
+    
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_admin', True)
+        return self.__create(username, password, **extra_fields)
+
+class CustomUser(AbstractUser, PermissionsMixin):
+
+    username = models.CharField(max_length=50, unique=True, null=False, db_index=True)
+    is_admin = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+    
+
